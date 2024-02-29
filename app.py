249693c5c8464ad4834635e559a219661efe61b5
thomasbulll <tombull66@hotmail.com>
2024-02-29 23:07:21 +0000

    Add modularity to HTML

--- app.py
+++ app.py
@@ -3,19 +3,20 @@ from news_scraper.aggregator import fetch_hacker_news_top_articles, fetch_bbc_to
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    hacker_news_articles = fetch_hacker_news_top_articles("https://news.ycombinator.com/")
-    bbc_news_articles = fetch_bbc_top_articles("https://www.bbc.co.uk/news/technology")
-    cnn_news_articles = fetch_cnn_top_articles("https://edition.cnn.com/business/tech")
-    business_insider_articles = fetch_business_insider_top_articles("https://www.businessinsider.com/tech")
-    tech_crunch_articles = fetch_tech_crunch_top_articles("https://techcrunch.com/")
+    news_sources = {
+        "Hacker News": fetch_hacker_news_top_articles("https://news.ycombinator.com/"),
+        "BBC News": fetch_bbc_top_articles("https://www.bbc.co.uk/news/technology"),
+        "CNN News": fetch_cnn_top_articles("https://edition.cnn.com/business/tech"),
+        "Business Insider": fetch_business_insider_top_articles(
+            "https://www.businessinsider.com/tech"
+        ),
+        "Tech Crunch": fetch_tech_crunch_top_articles("https://techcrunch.com/"),
+    }
 
-    return render_template('index.html', hacker_news_articles=hacker_news_articles, 
-                           bbc_news_articles=bbc_news_articles, 
-                           cnn_news_articles=cnn_news_articles, 
-                           business_insider_articles=business_insider_articles,
-                           tech_crunch_articles=tech_crunch_articles)
-        
+    return render_template("index.html", news_sources=news_sources)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
