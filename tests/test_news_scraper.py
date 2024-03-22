f8ffbec971d495fd33f1a69d8d2e60dd4d971af5
Thomas Bull <82104469+thomasbulll@users.noreply.github.com>
2024-03-22 16:13:12 +0000

    Add web tests to run alongside html tests

--- tests/test_news_scraper.py
+++ tests/test_news_scraper.py
@@ -16,50 +16,87 @@ def test_fetch_parsed_html_failure():
   soup = fetch_parsed_html(url)
   assert soup is None
   
-def test_fetch_bbc_top_articles():
+def test_fetch_html_bbc_top_articles():
     html_file = retrive_test_file_directory("test_bbc.html")
     articles = fetch_bbc_top_articles(html_file)
     assert len(articles) == 5
 
-def test_fetch_cnn_top_articles():
+def test_fetch_html_cnn_top_articles():
   html_file = retrive_test_file_directory("test_cnn.html")
   articles = fetch_cnn_top_articles(html_file)
   assert len(articles) == 5
 
-def test_fetch_hacker_news_top_articles():
+def test_fetch_html_hacker_news_top_articles():
   html_file = retrive_test_file_directory("test_hacker_rank.html")
   articles = fetch_hacker_news_top_articles(html_file)
   assert len(articles) == 5
 
-def test_fetch_business_insider_top_articless():
+def test_fetch_html_business_insider_top_articless():
   html_file = retrive_test_file_directory("business_insider.html")
   articles = fetch_business_insider_top_articles(html_file)
   assert len(articles) == 5
 
-def test_fetch_tech_crunch_top_articles():
+def test_fetch_html_tech_crunch_top_articles():
   html_file = retrive_test_file_directory("tech_crunch.html")
   articles = fetch_tech_crunch_top_articles(html_file)
   assert len(articles) == 5
 
-def test_fetch_sky_news_top_articles():
+def test_fetch_html_sky_news_top_articles():
   html_file = retrive_test_file_directory("test_sky_news.html")
   articles = fetch_sky_news_top_articles(html_file)
   assert len(articles) == 5
 
-def test_fetch_wired_top_articles():
+def test_fetch_html_wired_top_articles():
   html_file = retrive_test_file_directory("test_wired.html")
   articles = fetch_wired_top_articles(html_file)
   assert len(articles) == 5
 
-def test_fetch_venture_beat_top_articles():
+def test_fetch_html_venture_beat_top_articles():
   html_file = retrive_test_file_directory("test_venture_beat.html")
   articles = fetch_venture_beat_top_articles(html_file)
   assert len(articles) == 5
 
-def test_fetch_reddit_top_articles():
+def test_fetch_html_reddit_top_articles():
   html_file = retrive_test_file_directory("test_reddit.html")
   articles = fetch_reddit_top_articles(html_file, 0)
   assert len(articles) == 5
+  
+def test_fetch_bbc_top_articles():
+    articles = fetch_bbc_top_articles("https://www.bbc.co.uk/news/technology")
+    assert len(articles) == 5
+
+def test_fetch_cnn_top_articles():
+  articles = fetch_cnn_top_articles("https://edition.cnn.com/business/tech")
+  assert len(articles) == 5
+
+def test_fetch_hacker_news_top_articles():
+  articles = fetch_hacker_news_top_articles("https://news.ycombinator.com/")
+  assert len(articles) == 5
+
+def test_fetch_business_insider_top_articless():
+  articles = fetch_business_insider_top_articles("https://www.businessinsider.com/tech")
+  assert len(articles) == 5
+
+def test_fetch_tech_crunch_top_articles():
+  articles = fetch_tech_crunch_top_articles("https://techcrunch.com/")
+  assert len(articles) == 5
+
+def test_fetch_sky_news_top_articles():
+  articles = fetch_sky_news_top_articles("https://news.sky.com/technology/")
+  assert len(articles) == 5
+
+def test_fetch_wired_top_articles():
+  articles = fetch_wired_top_articles("https://www.wired.co.uk/topic/technology/")
+  assert len(articles) == 5
+
+def test_fetch_venture_beat_top_articles():
+  articles = fetch_venture_beat_top_articles("https://venturebeat.com/")
+  assert len(articles) == 5
+
+#Length here to be three not five due to dynamic loading
+def test_fetch_reddit_top_articles():
+  articles = fetch_reddit_top_articles("https://www.reddit.com/r/technews/", 0)
+  assert len(articles) == 3
 
 def test_fetch_reddit_top_articles_offset():
   html_file = retrive_test_file_directory("test_reddit.html")
